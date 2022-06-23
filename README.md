3fc50be55fdcc2ea773718ce2f2fc09f7086313d
JeanProfite <jeanprofite.ben@gmail.com>
2022-06-23 22:56:11 +0200

    Reorganisation generales: -Netoyage includes -Ajout de commantaires -Supression BLE

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ MotoLed give additional turn signals, brake lights and rear position lights with
 -WS2812B IP67 led strip
 
 MotoLed also include BME280 (Temperatur,Humidity,Pressure sensor) and come with a flutter based application to connect it with android and IOS devices.
-The app comunicate with ESP32 via bluethooth and receive the sensor readings and the state of the led strip. 
+The app comunicate with ESP32 via BLE and receive the sensor readings and the state of the led strip. 
 With it you can also change the led strip effect/color.
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,20 @@
 #include "FastLED.h"
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
-
-//------------------OLED Display-----------------------
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 #include <Adafruit_I2CDevice.h>
-#include <string.h>
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 //--------------------------------------------------------
+#define DEBUG           //Uncomment to activate debug
+
 #define NUM_LEDS 88     //Nombre de led 88
-#define DATA_PIN_LED 19 //Numéro du pin de données led
+#define DATA_PIN_LED 19 //Numéro du pin de données led 19
 #define LUM 255         //Luminosité led [0;255]
 #define Rp 10           //Intensité rouge feu position [0;255]
 #define Rs 255          //Intensité rouge feu stop [0;255]
-#define ClignoD_PIN 25 
-#define ClignoG_PIN 27
-#define Stop_PIN 32
-#define TriggerValue 30
+#define ClignoD_PIN 25  //Numéro du pin clignotant droit 25
+#define ClignoG_PIN 27  //Numéro du pin clignotant gauche 27
+#define Stop_PIN 32     //Numéro du pin feu stop 32
+#define TriggerValue 30 //Seuil declenchement
 
 CRGB leds[NUM_LEDS];
 float vPow = 3.3;     //Tension ADC ref
@@ -35,60 +23,15 @@ float r2 = 7500;      //Résitance pont2 div
 float Vstop = 0;      //Tension courante Feu stop
 float VclignoD = 0;   //Tension courante Clignotant droit
 float VclignoG = 0;   //Tension courante Clignotant gauche
-bool strob = 0;
-int etat = 0;     //Etat courant des led
-int R = 0;        //Valeur courante rouge led
-
-//----------------------------------------------------BLE-----------------------------------
-bool deviceConnected = false;
-
-void BLETransfer(int16_t);
-
-#define ServiceUUID "c9a110ba-7f3e-4165-847c-17f92e4ee6b3"
-#define DescriptorUUID BLEUUID((uint16_t)0x2901)
-
-#define temperatureCharacteristicUUID BLEUUID((uint16_t)0x2A6E)
-#define humidityCharacteristicUUID BLEUUID((uint16_t)0x2A6F)
-#define pressureCharacteristicUUID BLEUUID((uint16_t)0x2A6D)
-// --------------------------Characteristic definition----------------------------------------
-BLECharacteristic temperatureCharacteristic(
-    temperatureCharacteristicUUID,
-    BLECharacteristic::PROPERTY_READ |
-        BLECharacteristic::PROPERTY_NOTIFY);
-BLECharacteristic humidityCharacteristic(
-    humidityCharacteristicUUID,
-    BLECharacteristic::PROPERTY_READ |
-        BLECharacteristic::PROPERTY_NOTIFY);
-BLECharacteristic pressureCharacteristic(
-    pressureCharacteristicUUID,
-    BLECharacteristic::PROPERTY_READ |
-        BLECharacteristic::PROPERTY_NOTIFY);
-BLEDescriptor tempDescriptor(DescriptorUUID);
-BLEDescriptor humDescriptor(DescriptorUUID);
-BLEDescriptor pressureDescriptor(DescriptorUUID);
-
-//----------------------------BLE Callbacks------------------------------------
-
-class MyServerCallbacks : public BLEServerCallbacks
-{
-  void onConnect(BLEServer *pServer)
-  {
-    deviceConnected = true;
-    Serial.println("Device connected");
-  };
-
-  void onDisconnect(BLEServer *pServer)
-  {
-    deviceConnected = false;
-    Serial.println("Device disconnected");
-  }
-};
+bool strob = 0;       
+int etat = 0;         //Etat courant des led
+int R = 0;            //Valeur courante rouge led
 
 //----------------------------------------------------
 
 Adafruit_BME280 bme;
 unsigned long delayTime;
-int temperature = 0, humidity = 0, pressure = 0;
+float temperature = 0, humidity = 0, pressure = 0;
 
 void fill_half_grad(int Half, uint8_t Red, uint8_t Green, uint8_t Blue)
 {
@@ -123,72 +66,18 @@ void setup()
   timerAlarmEnable(timer);
 
   //----------------------------------------------------Serial
+  #ifdef DEBUG
   Serial.begin(115200);
   Serial.println("\n");
-  //----------------------------------------------------OLED
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
-  }
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-
-  //----------------------------------------------------BLE
-  // Create the BLE Device
-  BLEDevice::init("ESP");
-
-  // Create the BLE Server
-  BLEServer *pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
-
-  // Create the BLE Service
-  BLEService *pService = pServer->createService(ServiceUUID);
-
-  // Create a BLE Characteristic
-  //-----------------------------------------------------------------------
-  pService->addCharacteristic(&temperatureCharacteristic);
-  pService->addCharacteristic(&humidityCharacteristic);
-  pService->addCharacteristic(&pressureCharacteristic);
-
-  //-----------------------------------------------------------------------
-  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
-  // Create a BLE Descriptor
-  //-----------------------------------------------------------------------
-  tempDescriptor.setValue("Temperature -40-60°C");
-  temperatureCharacteristic.addDescriptor(&tempDescriptor);
-  temperatureCharacteristic.addDescriptor(new BLE2902());
-
-  humDescriptor.setValue("Humidity 0-100%");
-  humidityCharacteristic.addDescriptor(&humDescriptor);
-  humidityCharacteristic.addDescriptor(new BLE2902());
-
-  pressureDescriptor.setValue("pressure ATM (Pa)");
-  pressureCharacteristic.addDescriptor(&pressureDescriptor);
-  pressureCharacteristic.addDescriptor(new BLE2902());
-  //-----------------------------------------------------------------------
-  pServer->getAdvertising()->addServiceUUID(ServiceUUID);
-
-  // Start the service
-  pService->start();
-
-  // Start advertising
-  pServer->getAdvertising()->start();
-
-  Serial.println("Waiting for a Client to connect...");
-  
-  display.setCursor(0, 10);
-  display.println("Waiting for a Client to connect...");
-  display.display();
+  #endif
   //----------------------------------------------------BME280
   bool status;
   status = bme.begin(0x76);
-  /*if (!status)
+  if (!status)
   {
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1)
-      ;
-  }*/
+    //while (1);
+  }
   //----------------------LEDS---------------------------
   FastLED.addLeds<WS2812B, DATA_PIN_LED, RGB>(leds, NUM_LEDS);
   FastLED.setBrightness(LUM);
@@ -212,37 +101,25 @@ void setup()
 void loop()
 {
 
-//Envoie des mesures des capteurs si un client est connecté toute les x secondes (selon param timer)
-  if (deviceConnected && flagMeas == 1)
+//Mesure toute les x secondes (selon param timer)
+  if (flagMeas == 1)
   {
     temperature = bme.readTemperature() * 100;
     humidity = bme.readHumidity() * 100;
     pressure = bme.readPressure() * 100;
-
-    //char *numtStr = temperature;
-    //string strTemp = to_string(numStr);
-
-    Serial.printf("Pressure: %.2fhPa Temperature: %d°C Humidity: %d%% \n",pressure / 10,temperature / 100,humidity / 100);
-    Serial.printf("VclignoG: %f VclignoD: %f Vstop: %f \n",VclignoG,VclignoD,Vstop);
-
-    /* Set the value */
-    temperatureCharacteristic.setValue((uint8_t *)&temperature, size_t(2)); // This is a value of a single byte
-    temperatureCharacteristic.notify();                             // Notify the client of a change
-    humidityCharacteristic.setValue((uint8_t *)&humidity, size_t(2));       // This is a value of a single byte
-    humidityCharacteristic.notify();                                // Notify the client of a change
-    pressureCharacteristic.setValue((uint8_t *)&pressure, size_t(4));       // This is a value of a single byte
-    pressureCharacteristic.notify();                                // Notify the client of a change
     flagMeas = 0;
+    
+    #ifdef DEBUG
+    Serial.printf("Pressure: %.2fhPa Temperature: %.2f°C Humidity: %.2f%% \n",pressure/10 ,temperature/100 ,humidity/100);
+    //Serial.printf("VclignoG: %f VclignoD: %f Vstop: %f \n",VclignoG,VclignoD,Vstop);
+    #endif
   }
-  //display.clearDisplay();
-  //display.setCursor(0, 10);
-  //display.println("Temp:");
-  //display.display();
-  
-    Serial.printf("Pressure: $%.2fhPa Temperature: %d°C Humidity: %d%% \n",pressure / 10,temperature / 100,humidity / 100);
-    Serial.printf("VclignoG: $%d %d %d; VclignoD:  Vstop:  \n",(int)VclignoG
-    ,(int)VclignoD,(int)Vstop);
-    delay(10);
+
+  #ifdef DEBUG
+  //Serial.printf("Pressure: $%.2fhPa Temperature: %d°C Humidity: %d%% \n",pressure / 10,temperature / 100,humidity / 100);
+  //Serial.printf("VclignoG: $%d %d %d; VclignoD:  Vstop:  \n",(int)VclignoG,(int)VclignoD,(int)Vstop);
+  //delay(10);
+  #endif
   //Lecture des tensions
   float v0 = (analogRead(Stop_PIN) * vPow) / 1024.0;
   Vstop = v0 / (r2 / (r1 + r2));
@@ -252,30 +129,37 @@ void loop()
   VclignoG = v2 / (r2 / (r1 + r2));
   //Mise à jour de R
   R = (Vstop > TriggerValue) ? Rs : Rp;
+  
   if (strob == 1)
   {
     etat = 6; //Strob
   }
+
   else if (VclignoD > TriggerValue and VclignoG > TriggerValue)
   {
     etat = 1; //Warning
   }
+
   else if (VclignoD > TriggerValue and VclignoG < TriggerValue)
   {
     etat = 2; //Clignotant D
   }
+
   else if (VclignoG > TriggerValue and VclignoD < TriggerValue)
   {
     etat = 3; //Clignotants G
   }
+
   else if (Vstop > TriggerValue)
   {
     etat = 4; //Feu STOP Allumage
   }
+
   else if (Vstop < TriggerValue)
   {
     etat = 5; //Feu STOP Extinction
   }
+
 
   switch (etat)
   {
